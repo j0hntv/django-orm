8fa1ceb7b71362179aec434385757e783336fb34
J0hn <j0hntvdesign@gmail.com>
2019-12-18 14:39:56 +0300

    Update models.py

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -18,7 +18,7 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон')
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.PROTECT, verbose_name='Покемон')
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
     appeared_at = models.DateTimeField(blank=True, null=True, verbose_name='Время появления')
