a28a9dedf1f3b07df3c770e11c4cb084e355e9b9
J0hn <j0hntvdesign@gmail.com>
2020-04-06 19:31:19 +0300

    Add likes field to flat model

--- 05_Website_for_Realtors/property/models.py
+++ 05_Website_for_Realtors/property/models.py
@@ -22,8 +22,10 @@ class Flat(models.Model):
     has_balcony = models.NullBooleanField("Наличие балкона", db_index=True)
     active = models.BooleanField("Активно-ли объявление", db_index=True)
     construction_year = models.IntegerField("Год постройки здания", null=True, blank=True, db_index=True)
-
     new_building = models.NullBooleanField("Новостройка", db_index=True)
+
+    liked_by = models.ManyToManyField(User, related_name='liked_flats', verbose_name="Кто лайкнул")
+
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
