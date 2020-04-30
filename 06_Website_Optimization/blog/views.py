86b1a104d8596cf6d7789157a9b3f55f39c0243d
J0hn <j0hntvdesign@gmail.com>
2020-04-30 08:51:06 +0300

    Optimized main page

--- 06_Website_Optimization/blog/views.py
+++ 06_Website_Optimization/blog/views.py
@@ -29,7 +29,7 @@ def serialize_post_optimized(post):
         "image_url": post.image.url if post.image else None,
         "published_at": post.published_at,
         "slug": post.slug,
-        "tags": [serialize_tag(tag) for tag in post.tags.all()],
+        "tags": [serialize_tag_optimized(tag) for tag in Tag.objects.popular()],
         'first_tag_title': post.tags.all()[0].title,
     }
 
